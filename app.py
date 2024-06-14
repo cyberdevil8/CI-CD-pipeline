276180fb2b0f2dc6c285c714e5c1aecfa5540304
cyberdevil8 <amitpotdar31@gmail.com>
2024-06-14 18:10:55 +0530

    adding updated flask app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask 
+
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to simple flask web application"
+
+@app.route('/time')
+def showtime():
+    current_time = datetime.now().strftime("%H:%M:%S")
+    return f"Current server time is {current_time}"
+
+if __name__=='__main__':
+    app.run(debug=True, host='0.0.0.0')
